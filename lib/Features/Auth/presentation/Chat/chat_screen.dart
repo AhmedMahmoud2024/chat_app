eaf69a69c0f761815c3f988ef2e22adb08fcb21a
Ahmed Mahmoud <ahmedmahmoudsaleh269@gmail.com>
2026-03-16 00:09:51 -0700

    finished the implementation of chat screen

--- lib/Features/Auth/presentation/Chat/chat_screen.dart
+++ lib/Features/Auth/presentation/Chat/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ChatScreen extends StatefulWidget {
    ChatScreen({required this.receiverName, required this.receiverId});
@@ -62,27 +63,89 @@ class _ChatScreenState extends State<ChatScreen> {
       appBar: AppBar(
         title: Text(widget.receiverName)
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('chats').doc(
-          _getChatId()
-        ).collection('messages').orderBy(
-          'timeStamp',descending: true
-        ).snapshots(),
-         builder: (context,snapshot){
-          if(!snapshot.hasData ||snapshot.data!.docs.isEmpty){
-          return Center(child: Text('No messages yet ...'),);
-          } 
-          return ListView.builder(
-            controller: _scrollController,
-            reverse: true,
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance.collection('chats').doc(
+                _getChatId()
+              ).collection('messages').orderBy(
+                'timeStamp',descending: true
+              ).snapshots(),
+               builder: (context,snapshot){
+                if(!snapshot.hasData ||snapshot.data!.docs.isEmpty){
+                return Center(child: Text('No messages yet ...'),);
+                } 
+                return ListView.builder(
+                  controller: _scrollController,
+                  reverse: true,
+                  padding: EdgeInsets.all(16),
+                  itemCount: snapshot.data?.docs.length,
+                  itemBuilder:(context,index){
+                    final messageData=snapshot.data!.docs[index].data() as Map<String,dynamic>;
+                     final isMe= messageData['senderId'] ==currentUserId ;
+                     final timeStamp= messageData['timeStamp'] as Timestamp ;
+                     //need to install dateformat package
+                    final timeString = timeStamp!=null ?DateFormat('HH:mm').format(timeStamp.toDate()) : '';
+                return Align(alignment: isMe? Alignment.centerRight :Alignment.centerLeft,
+                  child: Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    padding: EdgeInsets.symmetric(horizontal: 16,vertical: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: isMe?Colors.blue :Colors.grey[300]
+                    ),
+                    child: Column(
+                      spacing: 5,
+                      crossAxisAlignment: CrossAxisAlignment.end, 
+                      children: [
+                     Text(messageData['text'],style: TextStyle(
+                      color: isMe? Colors.white :Colors.black
+                     ),), //meassage content
+                     Text(timeString) //uncommet after install package   
+                     
+                     
+                      ],
+                    ),
+                  ),
+                  );
+                  } 
+                  );
+               }
+               ),
+          ),
+          Container(
             padding: EdgeInsets.all(16),
-            itemCount: snapshot.data?.docs.length,
-            itemBuilder:(context,index){
-               
-            } 
-            );
-         }
-         ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey[200]!,
+                  spreadRadius: 1
+                  
+                )
+              ]
+            ),
+            child: SafeArea(child: Row(
+              children: [
+                Expanded(child: TextField(
+                  onSubmitted:(value) => _sendMessage(),
+                controller: _messageController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hint: Text('Enter your message ....')
+                ),
+                )),
+                CircleAvatar(
+                  backgroundColor: Colors.blue,
+                  child: IconButton(onPressed: _sendMessage, icon: 
+                  Icon(Icons.send,color: Colors.white,)),
+                )
+              ],
+            )),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/Features/Auth/presentation/login/login_page.dart
+++ lib/Features/Auth/presentation/login/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/Core/Network/firebase_auth_service.dart';
 import 'package:chat_app/Features/Auth/presentation/register/create_account_page.dart';
+import 'package:chat_app/Features/Users/users_screen.dart';
 import 'package:custom_form_w/custom_form_w.dart';
 import 'package:flutter/material.dart';
 
@@ -23,6 +24,7 @@ class LoginPage extends StatelessWidget {
                 emailController.text,
                 passwordController.text
               );
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>UsersScreen()));
             },
             children: [
             CustomTextField(
